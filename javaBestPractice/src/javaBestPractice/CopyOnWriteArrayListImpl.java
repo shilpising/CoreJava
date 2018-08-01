566f22a30185ac1b84bfdabdec67a0dd3db9c8d1
shilpising <35722185+shilpising@users.noreply.github.com>
2018-08-01 12:16:27 +0530

    CopyOnWriteArrayListImpl

---
+++ javaBestPractice/src/javaBestPractice/CopyOnWriteArrayListImpl.java
@@ -0,0 +1,28 @@
+package javaBestPractice;
+
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayListImpl {
+
+	public static void main(String[] args) {
+		CopyOnWriteArrayList<String> arrList=new CopyOnWriteArrayList<String>();
+		arrList.add("atharva");
+		arrList.add("shilpi");
+		arrList.add("avi");
+		arrList.add("vishu");
+		
+		Iterator<String> itr=arrList.iterator();
+		while(itr.hasNext()) {
+			System.out.printf("Read from CopyOnWriteArrayList : %s %n", itr.next());
+            arrList.remove("vishu"); //not supported in CopyOnWriteArrayList in Java
+		}
+		System.out.println("====================\n");
+		Iterator<String> itr1=arrList.iterator();
+		while(itr1.hasNext()) {
+			System.out.printf("Read from CopyOnWriteArrayList : %s %n", itr1.next());
+           // arrList.remove("vishu"); //not supported in CopyOnWriteArrayList in Java
+		}
+	}
+
+}
