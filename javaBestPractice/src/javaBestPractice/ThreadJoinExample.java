5a6bbf7a064ea0f5096689fdd19e1c486ea49646
shilpising <35722185+shilpising@users.noreply.github.com>
2019-09-25 17:50:37 +0530

    Update ThreadJoinExample.java

--- javaBestPractice/src/javaBestPractice/ThreadJoinExample.java
+++ javaBestPractice/src/javaBestPractice/ThreadJoinExample.java
@@ -18,7 +18,8 @@ public class ThreadJoinExample implements Runnable {
 			for(int i=0;i<10;i++) {
 				t[i]=new Thread(test,"thread"+i);
 				t[i].start();
-				t[i].join();
+				t[i].join();//If join() is called on a Thread instance, 
+				//the currently running thread will block until the Thread instance has finished executing.
 				
 			}
 		}catch(Exception e) {
